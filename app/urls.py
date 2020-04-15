d204e33b3b2f36a233c2a2bae3c72329dddc7bb5
Ramses <machiniram@gmail.com>
2020-04-15 12:10:04 -0500

    Union del frontend con backend

---
+++ app/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from django.contrib import admin
+from . import views
+from django.urls import path, re_path
+
+
+
+urlpatterns = [
+    path('',views.index),
+]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.core.paginator import Paginator
+from .models import *
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse, HttpResponse, HttpRequest, HttpResponseRedirect
+import sweetify
 # Create your views here.
+def index(request):
+    return render(request,'index.html')
